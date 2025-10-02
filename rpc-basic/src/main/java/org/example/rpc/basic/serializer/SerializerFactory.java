412e1b7e9cb894e7ea42739ad172475d6a0dfadf
baiyin <17693374267@163.com>
2025-10-02 21:18:19 +0800

    重构序列化器工厂，使用SPI加载器加载

--- rpc-basic/src/main/java/org/example/rpc/basic/serializer/SerializerFactory.java
+++ rpc-basic/src/main/java/org/example/rpc/basic/serializer/SerializerFactory.java
@@ -1,36 +1,27 @@
 package org.example.rpc.basic.serializer;
 
-import java.util.HashMap;
-import java.util.Map;
+import org.example.rpc.basic.spi.SpiLoader;
 
 /**
  * 序列化器工厂（用于获取序列化器对象）
  */
 public class SerializerFactory {
 
-    /**
-     * 序列化映射（用于实现单例）
-     */
-    private static final Map<String, Serializer> KEY_SERIALIZER_MAP = new HashMap<String, Serializer>() {{
-        put(SerializerKeys.JDK, new JdkSerializer());
-        put(SerializerKeys.JSON, new JsonSerializer());
-        put(SerializerKeys.KRYO, new KryoSerializer());
-        put(SerializerKeys.HESSIAN, new HessianSerializer());
-    }};
+    // 加载序列化器接口的所有实现类
+    static {
+        SpiLoader.load(Serializer.class);
+    }
 
     /**
      * 默认序列化器
      */
-    private static final Serializer DEFAULT_SERIALIZER = KEY_SERIALIZER_MAP.get(SerializerKeys.JDK);
+    public static final Serializer DEFAULT_SERIALIZER = new JdkSerializer();
 
     /**
      * 获取实例
-     *
-     * @param key
-     * @return
      */
     public static Serializer getInstance(String key) {
-        return KEY_SERIALIZER_MAP.getOrDefault(key, DEFAULT_SERIALIZER);
+        return SpiLoader.getInstance(Serializer.class, key);
     }
 
 }
