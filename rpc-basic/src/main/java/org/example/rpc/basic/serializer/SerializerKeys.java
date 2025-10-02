9112b67c9e35ade71d786ba3baf45696c20e90e0
baiyin <17693374267@163.com>
2025-10-02 21:03:25 +0800

    基于工厂模式和单例模式实现序列化器工厂

---
+++ rpc-basic/src/main/java/org/example/rpc/basic/serializer/SerializerKeys.java
@@ -0,0 +1,14 @@
+package org.example.rpc.basic.serializer;
+
+/**
+ * 序列化器键名
+ */
+public interface SerializerKeys {
+
+    String JDK = "jdk";
+    String JSON = "json";
+    String KRYO = "kryo";
+    String HESSIAN = "hessian";
+
+}
+
