f9cc2a9c828a25d238f4c7f2d2bd01a5642f5c60
baiyin <17693374267@163.com>
2025-10-02 21:33:50 +0800

    基于SPI加载器实现注册器工厂

---
+++ rpc-basic/src/main/java/org/example/rpc/basic/registry/RegistryFactory.java
@@ -0,0 +1,19 @@
+package org.example.rpc.basic.registry;
+
+import org.example.rpc.basic.spi.SpiLoader;
+
+// 注册中心工厂
+public class RegistryFactory {
+
+    static {
+        SpiLoader.load(Registry.class);
+    }
+
+    // 默认注册中心
+    private static final Registry DEFAULT_REGISTRY = new EtcdRegistry();
+
+    // 获取实例
+    public static Registry getInstance(String key) {
+        return SpiLoader.getInstance(Registry.class, key);
+    }
+}
