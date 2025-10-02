cac4c3b70c98b5f4e56c58d0d12be0f4047ffa07
baiyin <17693374267@163.com>
2025-10-03 00:52:40 +0800

    对于服务发布者，服务启动时注册到注册中心

--- service-provider/src/main/java/org/example/provider/ServiceProviderApplication.java
+++ service-provider/src/main/java/org/example/provider/ServiceProviderApplication.java
@@ -2,7 +2,11 @@ package org.example.provider;
 
 import org.example.common.service.UserService;
 import org.example.rpc.basic.RpcApplication;
-import org.example.rpc.basic.registry.LocalRegistry;
+import org.example.rpc.basic.config.RegistryConfig;
+import org.example.rpc.basic.config.RpcConfig;
+import org.example.rpc.basic.model.ServiceMetaInfo;
+import org.example.rpc.basic.registry.Registry;
+import org.example.rpc.basic.registry.RegistryFactory;
 import org.example.rpc.basic.server.HttpServer;
 import org.example.rpc.basic.server.VertxHttpServer;
 
@@ -11,11 +15,24 @@ public class ServiceProviderApplication {
         // RPC框架初始化
         RpcApplication.init();
 
-        // 应用启动时，注册提供的服务
-        LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
+        // 应用启动时，注册服务到注册中心
+        String userServiceName = UserService.class.getName();
+        RpcConfig rpcConfig = RpcApplication.getRpcConfig();
+        RegistryConfig registryConfig = rpcConfig.getRegistryConfig();
+        Registry registry = RegistryFactory.getInstance(registryConfig.getRegistry());
+        ServiceMetaInfo serviceMetaInfo = ServiceMetaInfo.builder()
+                .serviceName(userServiceName)
+                .serviceHost(rpcConfig.getServerHost())
+                .servicePort(rpcConfig.getServerPort())
+                .build();
+        try {
+            registry.register(serviceMetaInfo);
+        } catch (Exception e) {
+            throw new RuntimeException(String.format("向注册中心{%s}注册服务{%s}失败", registryConfig.getRegistry(), userServiceName), e);
+        }
 
         // 启动web服务
         HttpServer httpServer = new VertxHttpServer();
-        httpServer.doStart(RpcApplication.getRpcConfig().getServerPort());
+        httpServer.doStart(rpcConfig.getServerPort());
     }
 }
